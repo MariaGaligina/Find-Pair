2b698c3e63f79533ebfff4ed857b5e41700eb173
Maria Galigina <mashagaligina2003@gmail.com>
2024-08-28 22:08:54 +0300

    new actions to control the round

--- src/store/reducers/CardSlice.ts
+++ src/store/reducers/CardSlice.ts
@@ -8,6 +8,16 @@ interface CardState {
 	isRoundNow: boolean
 	isLoading: boolean
 	error: string
+	currentPair: currentPairParams[]
+	countOfPairsInRound: number
+	countOfFoundPairs: number
+	isVisible: boolean[]
+	isShaken: boolean[]
+}
+
+interface currentPairParams {
+	id: number
+	imgSrc: string
 }
 
 const initialState: CardState = {
@@ -16,6 +26,11 @@ const initialState: CardState = {
 	isRoundNow: false,
 	isLoading: false,
 	error: '',
+	currentPair: [],
+	countOfPairsInRound: 0,
+	countOfFoundPairs: 0,
+	isVisible: [],
+	isShaken: [],
 }
 
 export const cardSlice = createSlice({
@@ -34,12 +49,58 @@ export const cardSlice = createSlice({
 			state.isLoading = false
 			state.error = action.payload
 		},
+		initRound(state, action: PayloadAction<number>) {
+			state.roundCards = createRoundCards(state.cards, action.payload)
+			console.log('init round from init round')
+			state.isRoundNow = true
+			state.isVisible = new Array(action.payload * 2).fill(true)
+			state.isShaken = new Array(action.payload * 2).fill(false)
+			state.countOfPairsInRound = action.payload
+
+			console.log('timeout')
+			console.log('state.roundCards', state.roundCards)
+			console.log('state.isRoundNow', state.isRoundNow)
+			console.log('state.isVisible', state.isVisible)
+			console.log('state.isShaken', state.isShaken)
+			console.log('state.countOfPairsInRound', state.countOfPairsInRound)
+		},
+		endRound(state) {
+			state.roundCards = []
+			state.isRoundNow = false
+			state.isVisible = []
+			state.isShaken = []
+			state.countOfPairsInRound = 0
+			state.countOfFoundPairs = 0
+		},
 		initRoundCards(state) {
 			state.roundCards = createRoundCards(state.cards, 4)
 			console.log('init roundCards')
 		},
+		addCardToCheckPair(state, action: PayloadAction<currentPairParams>) {
+			console.log('action payload')
+			state.currentPair.push(action.payload)
+			console.log('current pair', state.currentPair)
+		},
+		deleteCardsToCheckPair(state) {
+			state.currentPair = []
+			console.log('deleted pair []', state.currentPair)
+		},
 		deleteCardsFromRound(state) {
 			state.roundCards = []
+		},
+		checkPair(state) {
+			if (state.currentPair[0].imgSrc === state.currentPair[1].imgSrc) {
+				if (state.currentPair[0].id === state.currentPair[1].id) {
+					console.log('this is the same card!!!!!!!')
+				} else {
+					console.log('cards are pair !!!!!!')
+					state.countOfFoundPairs += 1
+				}
+			} else {
+				console.log('cards not pair !!!!!!')
+			}
+			console.log(JSON.stringify(state.currentPair[0]))
+			console.log(JSON.stringify(state.currentPair[1]))
 		},
 		addCard(state, action: PayloadAction<ICard>) {
 			state.cards.push(action.payload)
