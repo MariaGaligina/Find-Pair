f57da1024e9aa9f24fdebf4d181d0fd8b099499f
Maria Galigina <mashagaligina2003@gmail.com>
2024-10-27 23:46:06 +0300

    feat(Modal.tsx):added function to close, styles, settings to modify modal window content 1. added option field result to show round result(block wid win or loss image) 2. added onClickOutsideContent to close modal when clicking outside 3. created variables for manage classes by cn 4. custom content depending on the result prop (presence of buttons, result images)

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,22 +1,72 @@
 import React, {ReactNode} from 'react'
 import styles from './Modal.module.scss'
+import cn from 'classnames'
 
 interface IChildrenProps {
 	children: ReactNode
 	isOpen: boolean
 	setIsOpen: React.Dispatch<React.SetStateAction<boolean>>
+	result?: string
 }
 
-const Modal: React.FC<IChildrenProps> = ({children, isOpen, setIsOpen}) => {
+const Modal: React.FC<IChildrenProps> = ({children, isOpen, setIsOpen, result}) => {
 	const onClick = () => {
 		setIsOpen((prev) => !prev)
 	}
+
+	const onClickOutsideContent = (event: Event) => {
+		const target = event.target as HTMLElement
+
+		if (target.classList) {
+			target.classList.forEach((style) => {
+				if (style.includes('modal-out')) {
+					setIsOpen((prev) => !prev)
+				}
+			})
+		}
+	}
+
+	const classesOutsideModalWindow = cn([styles['modal-out']], {
+		[styles['shadow-appearance']]: isOpen,
+		[styles['shadow-fade']]: !isOpen,
+	})
+
+	const classesModalWindowContent = cn([styles['modal-content']], {
+		[styles['content-block-appearance']]: isOpen,
+		[styles['content-block-fade']]: !isOpen,
+	})
+
+	const classesResultBlock = cn({
+		[styles.win]: typeof result !== undefined && result === 'win',
+		[styles.loss]: typeof result !== undefined && result === 'loss',
+	})
+
 	return (
-		<div className={`${styles['modal-out']} ${styles['shadow-appearance']}`}>
-			<div className={`${styles['modal-content']} ${styles['content-block-appearance']}`}>
+		<div
+			className={classesOutsideModalWindow}
+			onClick={() => {
+				onClickOutsideContent(event)
+			}}>
+			<div className={classesModalWindowContent}>
 				{children}
-				<button onClick={onClick}>Close</button>
+				{result !== '' ? (
+					''
+				) : (
+					<div className={styles.buttons}>
+						<button className={styles.button} onClick={onClick}>
+							Start
+						</button>
+						<button className={styles.button} onClick={onClick}>
+							Close
+						</button>
+					</div>
+				)}
 			</div>
+			{typeof result !== undefined && (result === 'win' || result === 'loss') ? (
+				<div className={classesResultBlock}></div>
+			) : (
+				''
+			)}
 		</div>
 	)
 }
