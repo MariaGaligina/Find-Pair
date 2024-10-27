0739e73c7b216416d11763a11a223321fab4f7b8
Maria Galigina <mashagaligina2003@gmail.com>
2024-10-28 00:13:27 +0300

    feat(Mode.tsx):added styles 1. added styles for component by emotion css 2. refactor code

--- src/components/Mode/Mode.tsx
+++ src/components/Mode/Mode.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import {Select, ISelectProps} from '../Select/Select'
+import {css} from '@emotion/css'
 
 interface IModeProps {
 	gameOptionsArray: Array<ISelectProps>
 }
 
 const Mode: React.FC<IModeProps> = ({gameOptionsArray}) => {
-	console.log(gameOptionsArray)
+	const modeStyles = css`
+		display: flex;
+		flex-direction: column;
+		gap: 10px;
+	`
+
 	return (
-		<div>
+		<div className={modeStyles}>
 			{gameOptionsArray.map((item, index) => (
 				<Select {...item} key={index} />
 			))}
