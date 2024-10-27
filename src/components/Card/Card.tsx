50a6b7928b9c1374ae9329d77746ce2cd73e2f5b
Maria Galigina <mashagaligina2003@gmail.com>
2024-10-27 22:07:23 +0300

    feat(Card.tsx): made the card double sided, added styles 1. added back and front card sides 2. created stylesheet variables using classnames library

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,31 +1,53 @@
-import {FC} from 'react'
+import {FC, useEffect} from 'react'
 import {ICard} from '../../models/ICard'
 import {css} from '@emotion/css'
+import cn from 'classnames'
+import styles from './Card.module.scss'
 
 interface CardProps {
-	imgSrc: string
 	id: number
-	onClick: (id: number, imgSrc: string) => void
-	isShaking: boolean
-	isVisible: boolean
+	imgSrc: string
+	onClick: (id: number, imgSrc: string, isClickable: boolean) => void
+	isClickable: boolean
+	isFound: boolean
+	isSelected: boolean
+	isShaken: boolean
 }
 
-const Card: FC<CardProps> = ({imgSrc, id, onClick}) => {
+const Card: FC<CardProps> = ({id, imgSrc, onClick, isClickable, isFound, isSelected, isShaken}) => {
+	const classesCard = cn([styles.card], {
+		[styles.not_clickable]: !isClickable,
+		[styles.not_clickable]: isFound,
+		//[styles.founded]: isFound,
+		//[styles.shaken]: isShaken,
+	})
+
+	const classesCardFront = cn([styles.card__front], {
+		[styles.not_clickable]: !isClickable,
+		[styles.selected_front]: isSelected || isShaken || isFound,
+	})
+	const classesCardBack = cn(
+		[styles.card__back],
+		{
+			[styles.selected_back]: isSelected || isShaken || isFound,
+		},
+		{
+			//[styles.not_clickable]: isClickable,
+			[styles.founded]: isFound,
+			[styles.shaken]: isShaken,
+		}
+	)
 	return (
-		<div
-			className={css`
-				max-width: 200px;
-			`}
-			onClick={() => onClick(id, imgSrc)}>
-			<img
-				src={imgSrc}
-				alt='not found'
-				className={css`
-					aspect-ratio: 1/1.5;
-					width: 100%;
-					height: 100%;
-					object-fit: cover;
-				`}></img>
+		<div className={classesCard} onClick={() => onClick(id, imgSrc, isClickable)}>
+			<div className={classesCardFront}>
+				<img
+					src='https://bogatyr.club/uploads/posts/2023-01/1675011031_bogatyr-club-p-fon-geometriya-fon-instagram-14.jpg'
+					alt='not found'
+				/>
+			</div>
+			<div className={classesCardBack}>
+				<img src={imgSrc} alt='not found' />
+			</div>
 		</div>
 	)
 }
