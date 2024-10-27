3bfdfdbb5c50b22ab331af2db252915767f7cf50
Maria Galigina <mashagaligina2003@gmail.com>
2024-10-28 00:35:26 +0300

    fix: modified toggling properties 1. toggling properties only for unique cards

--- src/functions/togglePropertyInCardsPair.ts
+++ src/functions/togglePropertyInCardsPair.ts
@@ -8,26 +8,36 @@ const togglePropertyInCardsPair = (
 ) => {
 	let clonedArray: ICard[] = JSON.parse(JSON.stringify(originalArray))
 	let clonedPair: currentPairParams[] = JSON.parse(JSON.stringify(comparisonArray))
+
 	if (!Object.keys(clonedArray[0]).includes(changedProperty)) {
 		return
 	} else {
-		const comparisonIds = clonedPair.map((item) => item.id)
+		let pairIds: number[] | string[] = clonedPair.map((item) => item.id)
+		let pairIdsSet = new Set(pairIds)
 
-		clonedArray = clonedArray.map((item) => {
-			if (comparisonIds.includes(item.id)) {
-				console.log(
-					changedProperty,
-					(item as {[key: string]: any})[changedProperty],
-					!(item as {[key: string]: any})[changedProperty]
-				)
-				return {
-					...item,
-					[changedProperty]: !(item as {[key: string]: any})[changedProperty],
+		if (pairIds.length !== pairIdsSet.size) {
+			clonedArray = clonedArray.map((item) => {
+				if (pairIdsSet.has(item.id)) {
+					return {
+						...item,
+						[changedProperty]: !(item as {[key: string]: any})[changedProperty],
+					}
+				} else {
+					return {...item}
 				}
-			} else {
-				return {...item}
-			}
-		})
+			})
+		} else {
+			clonedArray = clonedArray.map((item) => {
+				if (pairIds.includes(item.id)) {
+					return {
+						...item,
+						[changedProperty]: !(item as {[key: string]: any})[changedProperty],
+					}
+				} else {
+					return {...item}
+				}
+			})
+		}
 		return clonedArray
 	}
 }
