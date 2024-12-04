6173f98f82634ac62639bfa407b57446110a2131
Maria Galigina <mashagaligina2003@gmail.com>
2024-12-05 00:37:58 +0300

    fix/feat(CardSlice.tsx): added the main functionality of the game, fixed round control functions 1. added a pairType to the state for more convenient round management 2. (initRound): removed separate initialization of the isVisible and isShaken fields, now all necessary field values ​​for round cards are defined in the createRoundCards fu unction 3. (endRound): removed the separate clearing of the isVisible and isShaken fields, since now they are not in the state, instead of them there are Boolean properties separately in each card. These fields are cleared when cards from a round's card array are removed 4. removed the initRoundCards action, instead the createRoundCards function is used in the initRound action 5. added action checkIsRoundFinished for to track the end of the round 6. fixed the addCardToCheckPair action: removed the ability to select the same card in a pair twice. For cards that are in a pair, the isSelected property changes to true 7. added the deleteCardsToCheckPair action to clear the current pair in a round and manage card fields depending on their status 8. added a checkPair action to set the type of the currently selected pair (the main logic depends on this) 9. added the changeValuesOfPropertyInPairOfCards action to change the necessary properties in cards depending on their type (the necessary animations are triggered based on them) and update the number of cards found

--- src/store/reducers/CardSlice.ts
+++ src/store/reducers/CardSlice.ts
@@ -1,6 +1,8 @@
 import {ICard} from './../../models/ICard'
-import {createSlice, PayloadAction} from '@reduxjs/toolkit'
+import {createSlice, PayloadAction, createAsyncThunk} from '@reduxjs/toolkit'
 import createRoundCards from '../../functions/createRoundCards'
+import togglePropertyInCardsPair from '../../functions/togglePropertyInCardsPair'
+import togglePropertyInAllCards from '../../functions/togglePropertyInAllCards'
 
 interface CardState {
 	cards: ICard[]
@@ -11,8 +13,7 @@ interface CardState {
 	currentPair: currentPairParams[]
 	countOfPairsInRound: number
 	countOfFoundPairs: number
-	isVisible: boolean[]
-	isShaken: boolean[]
+	pairType: 'isNotPair' | 'isPair' | 'theSameCard' | 'notUsed'
 }
 
 export interface currentPairParams {
@@ -29,8 +30,7 @@ const initialState: CardState = {
 	currentPair: [],
 	countOfPairsInRound: 0,
 	countOfFoundPairs: 0,
-	isVisible: [],
-	isShaken: [],
+	pairType: 'notUsed',
 }
 
 export const cardSlice = createSlice({
@@ -51,56 +51,103 @@ export const cardSlice = createSlice({
 		},
 		initRound(state, action: PayloadAction<number>) {
 			state.roundCards = createRoundCards(state.cards, action.payload)
-			console.log('init round from init round')
 			state.isRoundNow = true
-			state.isVisible = new Array(action.payload * 2).fill(true)
-			state.isShaken = new Array(action.payload * 2).fill(false)
 			state.countOfPairsInRound = action.payload
-
-			console.log('timeout')
-			console.log('state.roundCards', state.roundCards)
-			console.log('state.isRoundNow', state.isRoundNow)
-			console.log('state.isVisible', state.isVisible)
-			console.log('state.isShaken', state.isShaken)
-			console.log('state.countOfPairsInRound', state.countOfPairsInRound)
+		},
+		checkIsRoundFinished(state, action: PayloadAction<number>) {
+			if (state.countOfFoundPairs === action.payload) {
+				console.log('РАУНД ЗАВЕРШАЕТСЯ, все найдено')
+			}
 		},
 		endRound(state) {
-			state.roundCards = []
 			state.isRoundNow = false
-			state.isVisible = []
-			state.isShaken = []
 			state.countOfPairsInRound = 0
 			state.countOfFoundPairs = 0
-		},
-		initRoundCards(state) {
-			state.roundCards = createRoundCards(state.cards, 4)
-			console.log('init roundCards')
-		},
-		addCardToCheckPair(state, action: PayloadAction<currentPairParams>) {
-			console.log('action payload')
-			state.currentPair.push(action.payload)
-			console.log('current pair', state.currentPair)
-		},
-		deleteCardsToCheckPair(state) {
-			state.currentPair = []
-			console.log('deleted pair []', state.currentPair)
-		},
-		deleteCardsFromRound(state) {
 			state.roundCards = []
 		},
-		checkPair(state) {
-			if (state.currentPair[0].imgSrc === state.currentPair[1].imgSrc) {
-				if (state.currentPair[0].id === state.currentPair[1].id) {
-					console.log('this is the same card!!!!!!!')
-				} else {
-					console.log('cards are pair !!!!!!')
-					state.countOfFoundPairs += 1
-				}
+		addCardToCheckPair(state, action: PayloadAction<currentPairParams>) {
+			state.currentPair.push(action.payload)
+			let cloneCurrentPair: currentPairParams[] = JSON.parse(JSON.stringify(state.currentPair))
+
+			if (cloneCurrentPair.length === 2 && cloneCurrentPair[0].id === cloneCurrentPair[1].id) {
+				return
 			} else {
-				console.log('cards not pair !!!!!!')
+				state.roundCards = togglePropertyInCardsPair(
+					state.roundCards,
+					[action.payload],
+					'isSelected'
+				)
 			}
-			console.log(JSON.stringify(state.currentPair[0]))
-			console.log(JSON.stringify(state.currentPair[1]))
+		},
+		deleteCardsToCheckPair(state) {
+			const notFoundRoundCards: currentPairParams[] = JSON.parse(
+				JSON.stringify(state.roundCards)
+			).filter((item: any) => {
+				if (item.isFound !== true) {
+					return {id: item.id, imgSrc: item.imgSrc}
+				}
+			})
+			//исключить кликабельность и невидимость найденных карт
+			state.roundCards = togglePropertyInCardsPair(
+				state.roundCards,
+				notFoundRoundCards,
+				'isClickable'
+			)
+			state.roundCards = togglePropertyInAllCards(state.roundCards, 'isShaken', false)
+			state.roundCards = togglePropertyInAllCards(state.roundCards, 'isSelected', false)
+
+			state.currentPair = []
+		},
+		checkPair(state) {
+			state.roundCards = togglePropertyInAllCards(state.roundCards, 'isClickable', false)
+
+			if (state.currentPair[0].imgSrc !== state.currentPair[1].imgSrc) {
+				state.pairType = 'isNotPair'
+			} else {
+				if (state.currentPair[0].id === state.currentPair[1].id) {
+					state.pairType = 'theSameCard'
+				} else {
+					state.pairType = 'isPair'
+				}
+			}
+		},
+		changeValuesOfPropertyInPairOfCards(state) {
+			state.roundCards = togglePropertyInCardsPair(
+				state.roundCards,
+				state.currentPair,
+				'isSelected'
+			)
+
+			switch (state.pairType) {
+				case 'isNotPair':
+					state.roundCards = togglePropertyInCardsPair(
+						state.roundCards,
+						state.currentPair,
+						'isShaken'
+					)
+					break
+				case 'theSameCard':
+					state.roundCards = togglePropertyInCardsPair(
+						state.roundCards,
+						state.currentPair,
+						'isShaken'
+					)
+
+					console.log(state.roundCards)
+					break
+				case 'isPair':
+					state.roundCards = togglePropertyInCardsPair(
+						state.roundCards,
+						state.currentPair,
+						'isFound'
+					)
+					state.countOfFoundPairs += 1
+					console.log('state.countOfFoundPairs', state.countOfFoundPairs)
+
+					break
+				case 'notUsed':
+					break
+			}
 		},
 		addCard(state, action: PayloadAction<ICard>) {
 			state.cards.push(action.payload)
